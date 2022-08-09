b563b8ce63c5d6cef503cc9fee5bf23d3d74b6f8
senthilsweb <senthilsweb@gmail.com>
2022-08-08 20:10:21 -0400

    Added glow button

--- components/Hero.vue
+++ components/Hero.vue
@@ -15,13 +15,19 @@
 		<div
 			class="text-center p-10 flex items-center justify-center space-x-4"
 		>
-			<NuxtLink
-				to="#"
-				@click="useNuxtApp().$bus.$emit('evtShowContactSales')"
-				class="px-6 py-3 text-sm bg-fuchsia-700 text-white rounded-full hover:bg-fuchsia-800 transition-all duration-300"
-				>Contact us</NuxtLink
-			>
-			<NuxtLink
+		<div class="relative group">
+      <div class="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+      <button 
+	 @click="useNuxtApp().$bus.$emit('evtShowContactSales')" 
+	  class="relative px-7 py-4 bg-black rounded-lg leading-none flex items-center divide-x divide-gray-600">
+        <span class="flex items-center space-x-5">
+          <span class="pr-6 text-gray-100">Contact us</span>
+        </span>
+        <span class="pl-6 text-indigo-400 group-hover:text-gray-100 transition duration-200">For new registrations &rarr;</span>
+      </button>
+    </div>
+			
+			<!--<NuxtLink
 				to="#"
 				class="text-sm group overflow-hidden relative bg-gray-50 ring-1 hover:ring-transparent ring-gray-300 rounded-full transition-all duration-300"
 			>
@@ -32,7 +38,7 @@
 					class="block px-6 py-3 transition-all duration-500 group-hover:text-white relative"
 					>Take Tour</span
 				>
-			</NuxtLink>
+			</NuxtLink>-->
 		</div>
 	</main>
 </template>
